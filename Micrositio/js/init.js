cdc8cb348158b8a8410e9d9282b6511f701f8fd8
Micropusheen <nancygarciadcv@gmail.com>
2017-06-28 18:39:18 -0500

    Add files via upload

---
+++ Micrositio/js/init.js
@@ -0,0 +1,23 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+<!-- slider-->     
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
+        
+
+
+
+
+       $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
